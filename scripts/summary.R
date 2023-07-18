5e0566e794ccaa75c6cb64add8da408e49dcfdb4
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-07-18 09:36:43 -0400

    Progress in processing uncertainty output.
    Put summation out of loop.

--- scripts/summary.R
+++ scripts/summary.R
@@ -1,6 +1,6 @@
 # Calculate total emission
 
-# Final values
+# Final emission values ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 dat.final <- dat.out[dat.out$time.hr == max(dat.out$time.hr), ]
 
 dat.final <- dat.final[order(dat.final$input.row.app), ]
@@ -12,8 +12,28 @@ dat.final$emis.n <- dat.final$app.tan * dat.final$er
 # Total. . .
 summ.year <- aggregate2(dat.final, c('app.tan', 'emis.n'), by = 'app.year', FUN = list(min = min, max = max, tot = sum, n = length))
 summ.year$ef <- summ.year$emis.n.tot / summ.year$app.tan.tot
-# Add in uncertainty results
-summ.year <- merge(summ.year, summ.uc, by = 'app.year')
+
+# Uncertainty results ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get totals by year and livestock category *before* taking quantiles
+summ1 <- aggregate2(dat.uc.out, c('app.man', 'app.tan', 'emis.n'), c('ucit', 'app.year', 'livestock.group'), FUN = list(sum))
+summ1$ef <- summ1$emis.n / summ1$app.tan 
+
+# And by year
+summ2 <- aggregate2(dat.uc.out, c('app.man', 'app.tan', 'emis.n'), c('ucit', 'app.year'), FUN = list(sum))
+summ2$ef <- summ2$emis.n / summ2$app.tan 
+
+# Now quantiles 
+# By year
+summ.uc.yr <- aggregate2(summ2, c('app.man', 'app.tan', 'emis.n', 'ef'), c('app.year'), 
+                         FUN = list(lwr = function(x) quantile(x, (1 - cl) / 2), upr = function(x) quantile(x, 0.5 + cl / 2)))
+
+# By year and livestock category
+summ.uc.yr.lv <- aggregate2(summ1, c('app.man', 'app.tan', 'emis.n', 'ef'), c('app.year', 'livestock.group'), 
+                           FUN = list(lwr = function(x) quantile(x, (1 - cl) / 2), upr = function(x) quantile(x, 0.5 + cl / 2)))
+
+
+# Combined results ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+summ.year <- merge(summ.year, summ.yr.uc, by = 'app.year')
 # Get relative uncertainty
 summ.year$rlwr <- (summ.year$emis.n.tot - summ.year$emis.n.lwr) / summ.year$emis.n.tot
 summ.year$rupr <- (summ.year$emis.n.upr - summ.year$emis.n.tot) / summ.year$emis.n.tot
@@ -25,4 +45,5 @@ summ.loc.year$ef <- summ.loc.year$emis.n.tot / summ.loc.year$app.tan.tot
 # By aggregation groups
 summ.agg1.year <- aggregate2(dat.final, c('app.tan', 'emis.n'), by = c('loc.agg1', 'app.year'), FUN = list(min = min, max = max, tot = sum))
 summ.agg1.year$ef <- summ.agg1.year$emis.n.tot / summ.agg1.year$app.tan.tot
+
 
--- scripts/uncert.R
+++ scripts/uncert.R
@@ -7,7 +7,7 @@ if (settings[['uncert']] == 'Yes' | settings[['paruncert']] == 'Yes') {
   nu <- settings[['nu']]
   cl <- settings[['cl']]
   
-  dat.tot <- data.frame()
+  dat.uc.out <- data.frame()
   
   logmssg(paste0('Starting ', nu, ' uncertainty iterations . . .'), logfile = logfn)
 
@@ -55,19 +55,17 @@ if (settings[['uncert']] == 'Yes' | settings[['paruncert']] == 'Yes') {
   
     # Combine emission predictions with inputs for summarizing
     dd <- cbind(dat.uc, emis[, c('e', 'er')])
-    # Total emission of NH3-N 
+
+    # Get emission
     dd$emis.n <- dd$app.tan * dd$er
-    dat.tot <- rbind(dat.tot, aggregate(dd[, 'emis.n', drop = FALSE], dd[, 'app.year', drop = FALSE], sum))
+    dd$ucit <- i
+
+    dat.uc.out <- rbind(dat.uc.out, dd)
   }
-  
-  summ.uc <- aggregate2(dat.tot, 'emis.n', 'app.year', FUN = list(q = function(x) quantile(x, c((1 - cl) / 2, 0.5 + cl / 2))))
-  lwr <- aggregate(dat.tot[, 'emis.n', drop = FALSE], dat.tot[, 'app.year', drop = FALSE], quantile, c((1 - cl) / 2))
-  upr <- aggregate(dat.tot[, 'emis.n', drop = FALSE], dat.tot[, 'app.year', drop = FALSE], quantile, c(0.5 + cl / 2))
-  summ.uc <- merge(lwr, upr, by = 'app.year')
-  names(summ.uc)[2:3] <- c('emis.n.lwr', 'emis.n.upr')
 
   logmssg('Done\n', logfile = logfn)
 
 } else {
-  summ.uc <- data.frame(app.year = sort(unique(dat.in$app.year)), emis.n.lwr = NA, emis.n.upr = NA)
+  dat.uc.out <- dat.in
+  dat.uc.out$ucit <- 0
 }
